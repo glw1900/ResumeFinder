a37ca3969932a0c9224bce659b7a395dd0ad5863
playernovice <wxy8711@gmail.com>
2015-10-17 00:17:23 -0400

    new copy hash modification

--- process.rb
+++ process.rb
@@ -1,9 +1,16 @@
 def check_pwd (hsh)
   if hsh[:password] == hsh[:re_password]
-    hsh.delete(:re_password)
-    hsh[:gpa].to_f
-    return hsh
+    new_hsh = hsh.clone
+    new_hsh.delete(:re_password)
+    new_hsh[:gpa].to_f
+    return new_hsh
   else
     return nil
   end
 end
+
+
+hsh = {"password":"abc","re_password":"abc","gpa":4.0}
+h = check_pwd(hsh)
+puts (hsh)
+puts (h)
