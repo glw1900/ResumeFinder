9daa595b3dbc2e54ac3545c35d6b4cd2c9b00d2c
glw1900 <guoliangwei1900@gmail.com>
2015-10-16 22:37:34 -0400

    change app.rb

--- app.rb
+++ app.rb
@@ -2,7 +2,19 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require './config/environments' # database configuration
 
-get '/'
-    
+get '/' do
+  erb :home
 end
 
+get '/register' do
+  
+end
+
+post "/submit_regis_app" do
+  @applicant = Applicant.new(params[:regis])
+  if @applicant.save:
+    redirect '/'
+  else:
+    "Something is Wrong"
+  end
+end
