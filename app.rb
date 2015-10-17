9335847d9e013fe064685195880c7a5951ce396a
glw1900 <guoliangwei1900@gmail.com>
2015-10-16 22:50:06 -0400

    add two registrations page

--- app.rb
+++ app.rb
@@ -6,15 +6,24 @@ get '/' do
   erb :home
 end
 
-get '/register' do
-  
+get "/register" do
+  erb :regis_app
 end
 
 post "/submit_regis_app" do
   @applicant = Applicant.new(params[:regis])
-  if @applicant.save:
+  if @applicant.save
     redirect '/'
-  else:
+  else
+    "Something is Wrong"
+  end
+end
+
+post "/submit_regis_com" do
+  @company = Company.new(param[:regis])
+  if @company.save
+    redirect '/'
+  else
     "Something is Wrong"
   end
 end
