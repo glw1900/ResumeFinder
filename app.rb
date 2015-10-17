b520e7ec94ca7549f415366bb9911538ddd362b6
glw1900 <guoliangwei1900@gmail.com>
2015-10-16 23:05:30 -0400

    update app.rb

--- app.rb
+++ app.rb
@@ -1,9 +1,25 @@
+require 'sinatra'
+require 'sinatra/activerecord'
+require './config/environments'
+
 get '/' do
   erb :home
 end
 
-get "/register" do
+get "/app_register" do
   erb :regis_app
+end
+
+get "/com_register" do
+  erb :regis_com
+end
+
+post "/app_login" do
+  
+end
+
+post "/com_login" do
+  
 end
 
 post "/submit_regis_app" do
