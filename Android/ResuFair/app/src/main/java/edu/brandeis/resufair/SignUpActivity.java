7cc3484d3391fbadb63eb490382696141adda6b7
glw1900 <guoliangwei1900@gmail.com>
2015-10-20 16:42:50 -0400

    add SignUpActivity

--- Android/ResuFair/app/src/main/java/edu/brandeis/resufair/SignUpActivity.java
+++ Android/ResuFair/app/src/main/java/edu/brandeis/resufair/SignUpActivity.java
@@ -1,11 +1,13 @@
 package edu.brandeis.resufair;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -13,17 +15,21 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
     }
 
+    protected void authenticate(View view) {
+        EditText userEmail = (EditText) findViewById(R.id.userEmail);
+        EditText userPassword = (EditText) findViewById(R.id.userPassword);
+        EditText userRepassword = (EditText) findViewById(R.id.reUserPassword);
+        if (userPassword.equals(userRepassword)){
+            //write to database
+            this.finish();
+        } else {
+            userPassword.setText("");
+            userRepassword.setText("");
+        }
+    }
 }
+
