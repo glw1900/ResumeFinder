37d3f8fb8b06b55ceb095c1340595dce4f0abab1
glw1900 <guoliangwei1900@gmail.com>
2015-10-20 17:29:18 -0400

    add signup activity

--- Android/ResuFair/app/src/main/java/edu/brandeis/resufair/SignUpActivity.java
+++ Android/ResuFair/app/src/main/java/edu/brandeis/resufair/SignUpActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
+import android.content.Context;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -19,16 +21,22 @@ public class SignUpActivity extends AppCompatActivity {
 //        setSupportActionBar(toolbar);
     }
 
-    protected void authenticate(View view) {
-        EditText userEmail = (EditText) findViewById(R.id.userEmail);
-        EditText userPassword = (EditText) findViewById(R.id.userPassword);
-        EditText userRepassword = (EditText) findViewById(R.id.reUserPassword);
-        if (userPassword.equals(userRepassword)){
+    public void authenticate(View view) {
+        EditText userEmail = (EditText) findViewById(R.id.userEmail_regis);
+        EditText userPassword = (EditText) findViewById(R.id.userPassword_regis);
+        EditText userRepassword = (EditText) findViewById(R.id.reUserPassword_regis);
+        if (userPassword.getText().toString().equals(userRepassword.getText().toString())){
             //write to database
             this.finish();
         } else {
             userPassword.setText("");
             userRepassword.setText("");
+
+            Context context = getApplicationContext();
+            CharSequence text = "Password not match,try again.";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
         }
     }
 }
