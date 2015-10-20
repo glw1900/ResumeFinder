6d9397b56aef8b009dea8cb42ab3e2ca5cf8fc02
playernovice <wxy8711@gmail.com>
2015-10-20 17:41:41 -0400

    status

--- Android/ResuFair/app/src/main/java/edu/brandeis/resufair/StatusActivity.java
+++ Android/ResuFair/app/src/main/java/edu/brandeis/resufair/StatusActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import java.util.HashMap;
 import android.view.View;
 import android.widget.TextView;
 
@@ -25,13 +27,27 @@ public class StatusActivity extends AppCompatActivity {
 
         // Get the message from the intent
         Intent intent = getIntent();
-        String[] message = intent.getStringArrayExtra(MainActivity.USER_INFO);
+        HashMap<String, String> hashMap = (HashMap<String, String>)intent.getSerializableExtra("map");
+        //   Log.v("HashMapTest", hashMap.get("key"));
 
-        TextView username = (TextView) findViewById(R.id.status_username);
-        TextView password = (TextView) findViewById(R.id.status_password);
+        TextView name = (TextView) findViewById(R.id.status_name);
+        TextView birthday = (TextView) findViewById(R.id.status_birthday);
+        TextView college = (TextView) findViewById(R.id.status_college);
+        TextView major = (TextView) findViewById(R.id.status_major);
+        TextView gpa = (TextView) findViewById(R.id.status_gpa);
 
-        username.setText(message[0]);
-        password.setText(message[1]);
+        name.setText(hashMap.get("name"));
+        birthday.setText(hashMap.get("birthday"));
+        college.setText(hashMap.get("college"));
+        major.setText(hashMap.get("major"));
+        gpa.setText(hashMap.get("gpa"));
+//        String[] message = intent.getStringArrayExtra(MainActivity.USER_INFO);
+//
+//        TextView username = (TextView) findViewById(R.id.status_username);
+//        TextView password = (TextView) findViewById(R.id.status_password);
+//
+//        username.setText(message[0]);
+//        password.setText(message[1]);
     }
 
 }
