446c13b98a89cb8d8796333de538863573291a1e
CassyLee <cassyljs@126.com>
2015-10-30 23:06:34 -0400

    database

--- Server/db/migrate/20151017030730_create_model.rb
+++ Server/db/migrate/20151017030730_create_model.rb
@@ -5,20 +5,40 @@ class CreateModel < ActiveRecord::Migration
       applicant.string :password
       applicant.string :first_name
       applicant.string :last_name
-      applicant.string :school
-      applicant.string :degree
-      applicant.float :gpa
+      applicant.string :summary
     end
+
     create_table :companies do |company|
       company.string :email
       company.string :password
       company.string :name
       company.string :info
     end
+
     create_table :projects do |project|
       project.integer :appl_id
+      project.string :title
+      project.date :start_date
+      project.date :end_date
       project.string :describe
     end
+
+    create_table :educations do |education|
+      education.integer :appl_id
+      education.string :title
+      education.string :describe
+      education.date :start_date
+      education.date :end_date
+    end
+
+    create_table :experiences do |experience|
+      experience.integer :appl_id
+      experience.string :title
+      experience.string :describe
+      experience.date :start_date
+      experience.date :end_date
+    end
+
     create_table :receives do |receive|
       receive.integer :appl_id
       receive.integer :comp_id
@@ -29,6 +49,8 @@ class CreateModel < ActiveRecord::Migration
     drop_table :applicants
     drop_table :companies
     drop_table :projects
+    drop_table :educations
+    drop_table :experiences
     drop_table :receives
   end
 end
